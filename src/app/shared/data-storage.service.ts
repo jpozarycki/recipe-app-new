a55451933487bd760c294b4064a60f96a27b85ef
jpozarycki <jakub.pozarycki@gmail.com>
2019-06-05 10:17:24 +0200

    Added interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,28 +1,34 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Recipe} from '../recipes/recipe.model';
 import {RecipeService} from '../recipes/recipe.service';
-import {map, tap} from 'rxjs/operators';
+import {exhaustMap, map, take, tap} from 'rxjs/operators';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
 
-  constructor(private http: HttpClient, private recipeService: RecipeService) { }
+  constructor(private http: HttpClient, private recipeService: RecipeService) {
+  }
 
   fetchRecipes() {
-    return this.http.get<Recipe[]>('https://ng-recipe-app-3a48f.firebaseio.com/recipes.json').pipe(map(
-    recipes => {
-      return recipes.map(recipe => {
-        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-      });
-    }),
-      tap(recipes => {
-        this.recipeService.setRecipes(recipes);
-        console.log(recipes);
-      })
-    );
+    return this.http.get<Recipe[]>('https://ng-recipe-app-3a48f.firebaseio.com/recipes.json?')
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+          console.log(recipes);
+        })
+      );
+
   }
 
   storeRecipes() {
