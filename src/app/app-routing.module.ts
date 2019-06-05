c7f1fbbf2ef36fca09004154e56f9a2cb4e02d78
jpozarycki <jakub.pozarycki@gmail.com>
2019-06-05 17:42:52 +0200

    Added lazy loading for recipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
-import {AuthComponent} from './auth/auth.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'sign-up', component: AuthComponent}
-];
+  {path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'}
+  ];
 
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,31 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import {ShoppingListModule} from './shopping-list/shopping-list.module';
+import {SharedModule} from './shared/shared.module';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
-import {RecipeService} from './recipes/recipe.service';
-import {AppRoutingModule} from './app-routing.module';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {AuthComponent} from './auth/auth.component';
-import {AuthInterceptorService} from './auth/auth-interceptor.service';
 import {AlertComponent} from './shared/alert/alert.component';
-import {RecipesModule} from './recipes/recipes.module';
-import {ShoppingListModule} from './shopping-list/shopping-list.module';
-import {SharedModule} from './shared/shared.module';
+import {CoreModule} from './core.module';
+import {AuthModule} from './auth/auth.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    AuthComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
-    RecipesModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    CoreModule,
+    AuthModule
   ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    {
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptorService,
-    multi: true
-  }],
   bootstrap: [AppComponent],
   entryComponents: [
     AlertComponent
