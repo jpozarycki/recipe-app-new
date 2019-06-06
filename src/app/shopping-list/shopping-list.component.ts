6121b2edd2e992beecb38aafe03f545c86ce3dcb
jpozarycki <jakub.pozarycki@gmail.com>
2019-06-06 16:30:12 +0200

    Deleted shopping list entirely

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import {Ingredient} from '../shared/ingredient.model';
-import {ShoppingListService} from './shopping-list.service';
 import {Observable, Subscription} from 'rxjs';
 import {LoggingService} from '../logging.service';
 import {Store} from '@ngrx/store';
@@ -18,7 +17,6 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   private subscription: Subscription;
 
   constructor(
-    private slService: ShoppingListService,
     private loggingService: LoggingService,
     private store: Store<fromShoppingList.AppState>
   ) {
