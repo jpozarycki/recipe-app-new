b4f3a7b8f5c767228889049fc0b1ea3ee3125446
jpozarycki <jakub.pozarycki@gmail.com>
2019-06-06 18:35:25 +0200

    Implemented fully auth reducer and actions

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,10 @@
-import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {DataStorageService} from '../shared/data-storage.service';
 import {AuthService} from '../auth/auth.service';
 import {Subscription} from 'rxjs';
+import {Store} from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -12,10 +15,11 @@ export class HeaderComponent implements OnInit, OnDestroy {
   isAuthenticated = false;
   private userSub: Subscription;
 
-  constructor(private dsService: DataStorageService, private authService: AuthService) {}
+  constructor(private dsService: DataStorageService, private authService: AuthService, private store: Store<fromApp.AppState>) {
+  }
 
   ngOnInit(): void {
-    this.userSub = this.authService.user.subscribe(user => {
+    this.userSub = this.store.select('auth').pipe(map(authState => authState.user)).subscribe(user => {
       this.isAuthenticated = !!user;
     });
   }
