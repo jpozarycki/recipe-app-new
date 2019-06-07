727f5ee399a85dd3d5591059d6e3f96850828369
jpozarycki <jakub.pozarycki@gmail.com>
2019-06-07 11:21:59 +0200

    Implemented NgRx Effects for login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -5,6 +5,9 @@ import {Observable, Subscription} from 'rxjs';
 import {Router} from '@angular/router';
 import {AlertComponent} from '../shared/alert/alert.component';
 import {PlaceholderDirective} from '../shared/placeholder/placeholder.directive';
+import {Store} from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 @Component({
   selector: 'app-auth',
@@ -19,13 +22,26 @@ export class AuthComponent implements OnInit, OnDestroy {
   @ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective;
 
   private closeSub: Subscription;
-  constructor(private authService: AuthService, private router: Router, private compFactoryResolver: ComponentFactoryResolver) { }
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private compFactoryResolver: ComponentFactoryResolver,
+    private store: Store<fromApp.AppState>) {
+  }
 
   onSwitch() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   ngOnInit() {
+    this.store.select('auth').subscribe(authState => {
+      this.isLoading = authState.loading;
+      this.error = authState.authError;
+      if (this.error) {
+        this.showErrorAlert(this.error);
+      }
+    });
   }
 
   onSubmit(authForm: NgForm) {
@@ -39,20 +55,21 @@ export class AuthComponent implements OnInit, OnDestroy {
     let authObs: Observable<AuthResponseData>;
     this.isLoading = true;
     if (this.isLoginMode) {
-      authObs = this.authService.logIn(email, password);
+      // authObs = this.authService.logIn(email, password);
+      this.store.dispatch(new AuthActions.LoginStart({email, password}));
     } else {
-       authObs = this.authService.signUp(email, password);
+      authObs = this.authService.signUp(email, password);
     }
 
-    authObs.subscribe(resData => {
-      console.log(resData);
-      this.isLoading = false;
-      this.router.navigate(['/recipes']);
-    }, errorMessage => {
-      this.error = errorMessage;
-      this.showErrorAlert(errorMessage);
-      this.isLoading = false;
-    });
+    // authObs.subscribe(resData => {
+    //   console.log(resData);
+    //   this.isLoading = false;
+    //   this.router.navigate(['/recipes']);
+    // }, errorMessage => {
+    //   this.error = errorMessage;
+    //   this.showErrorAlert(errorMessage);
+    //   this.isLoading = false;
+    // });
     authForm.reset();
   }
 
